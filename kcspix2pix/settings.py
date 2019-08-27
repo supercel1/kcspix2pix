af2e2fa35d12fcb8eac420ddf17db86b30df31df
supercell <undermountainafu@icloud.com>
2019-08-27 18:53:11 +0900

    update templates

--- kcspix2pix/settings.py
+++ kcspix2pix/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'kcspix2pix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'kcspix2pix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'images',
+        'USER': 'supercell',
+        'PASSWORD': 'supercel1user',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
