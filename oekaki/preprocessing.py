5695245e4dddfe27affd41452b2742c8a588c563
supercell <undermountainafu@icloud.com>
2019-09-10 05:09:05 +0900

    JPEG画像前処理追加

--- oekaki/preprocessing.py
+++ oekaki/preprocessing.py
@@ -17,10 +17,23 @@ class ImageTransform():
     def __call__(self, img):
         return self.transform(img)
 
-def make_input(img_path):
-    img = Image.open(img_path).convert('RGB')
+def make_input(img_path, img_name):
+    img = to_RGB(Image.open(img_path), img_name)
+    print(img)
 
     transform = ImageTransform(resize, mean, std)
     img_transformed = transform(img)
     img_input = img_transformed.unsqueeze_(0) # ミニバッチ分の次元を追加
     return img_input
+
+def to_RGB(img, img_name):
+    if img.mode == 'RGB':
+        return img
+    
+    img.load()
+    background = Image.new('RGB', img.size, (255, 255, 255))
+    background.paste(img, mask=img.split()[3])
+
+    file_name = img_name + '.jpg'
+    background.save(file_name, 'JPEG', quality=80)
+    return Image.open(file_name)
--- oekaki/views.py
+++ oekaki/views.py
@@ -8,9 +8,10 @@ import json
 import numpy as np
 from PIL import Image
 import io
+import matplotlib.pyplot as plt
 
 from .networks import CycleGAN
-from .models import Images, Files, FakeImage
+from .models import Images, File, FakeImage
 from .preprocessing import make_input
 
 # Create your views here.
@@ -26,17 +27,18 @@ def predict_image(request):
     img_byte = request.body
     img_base64 = img_byte.decode(encoding='utf-8')
     img_data = base64.b64decode(img_base64)
-    img_name = str(uuid.uuid4()) + '.png'
-    image = ContentFile(img_data, img_name)
+    img_name = str(uuid.uuid4())
+    image = ContentFile(img_data, img_name + '.png')
 
     Images.objects.create(image=image)
-    image_path = 'media/pix2pix/' + img_name
+    image_path = 'media/pix2pix/' + img_name + '.png'
 
-    img_input = make_input(image_path)
+    img_input = make_input(image_path, img_name)
     fake_Y = model.G_X(img_input)
 
-    fake_img_np = fake_Y[0].detach().numpy().transpose((1, 2, 0))
-    fake_img_np = np.clip(fake_img_np, 0, 1)
+    fake_img_np = fake_Y[0].detach().numpy()
+    fake_img_np = 0.5 * (fake_img_np + 1)
+    fake_img_np = np.transpose(fake_img_np, (1, 2, 0))
 
     fake_img_pil = Image.fromarray((fake_img_np * 255).astype(np.uint8))
 
@@ -49,6 +51,8 @@ def predict_image(request):
     FakeImage.objects.create(fake_image=fake_image)
     fake_image_path = 'media/pix2pix/fake/' + fake_img_name
 
+    fake_ = Image.open(fake_image_path)
+    print(fake_.convert('RGB'))
 
     context = {'fake_image_path': fake_image_path }
     return JsonResponse(context)
@@ -58,8 +62,7 @@ def predict_file(request):
     model.log_dir = 'logs'
     model.load('epoch195')
 
-    form = Files(request.POST, request.FILES)
-    form.save()
+    print(request.POST)
 
-    context = {'status': '200 OK'}
+    context = {'form': 'aaa' }
     return JsonResponse(context)
