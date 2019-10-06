cc79292adbcf6da442e99e4971546325b4475067
supercell <undermountainafu@icloud.com>
2019-10-06 22:55:51 +0900

    deploy

--- kcspix2pix/settings.py
+++ kcspix2pix/settings.py
@@ -19,15 +19,14 @@ try:
 except ImportError:
     pass
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 if not DEBUG:
     import django_heroku
 
     SECRET_KEY = os.environ["SECRET_KEY"]
     django_heroku.settings(locals())
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
